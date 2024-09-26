d3d3ab76d6121463418e054ae83ad1b331aba7ab
wasupalonely <juandiegomaciasoy@gmail.com>
2024-09-26 17:12:15 -0500

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
